a842aac9010f45101f9f140a272dc69cc25f1b3a
xiaochun <xchun90@163.com>
2024-06-25 21:12:45 +0800

    Add title

--- src/main/java/com/zrlog/plugin/article/arranger/GraalvmAgentApplication.java
+++ src/main/java/com/zrlog/plugin/article/arranger/GraalvmAgentApplication.java
@@ -2,13 +2,19 @@ package com.zrlog.plugin.article.arranger;
 
 import com.google.gson.Gson;
 import com.zrlog.plugin.article.arranger.controller.ArticleArrangerController;
+import com.zrlog.plugin.article.arranger.vo.ArrangeOutlineVO;
+import com.zrlog.plugin.article.arranger.vo.ArticleInfo;
 import com.zrlog.plugin.common.PluginNativeImageUtils;
 import com.zrlog.plugin.data.codec.HttpRequestInfo;
 import com.zrlog.plugin.message.Plugin;
+import com.zrlog.plugin.render.FreeMarkerRenderHandler;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 public class GraalvmAgentApplication {
 
@@ -21,7 +27,22 @@ public class GraalvmAgentApplication {
         PluginNativeImageUtils.doLoopResourceLoad(file.listFiles(), file.getPath()  + "/", "/");
         PluginNativeImageUtils.exposeController(Collections.singletonList(ArticleArrangerController.class));
         PluginNativeImageUtils.usedGsonObject();
+        freemarkerInit();
+        new Gson().toJson(new ArrangeOutlineVO());
+        new Gson().toJson(new ArticleInfo());
         Application.main(args);
 
+    }
+
+    private static void freemarkerInit(){
+        Plugin plugin = new Plugin();
+        plugin.setName("test");
+        plugin.setDesc("test");
+        plugin.setVersion("test");
+        Map<String, Object> objectObjectHashMap = new HashMap<>();
+        objectObjectHashMap.put("styleGlobal", "");
+        objectObjectHashMap.put("mainColor", "");
+        objectObjectHashMap.put("groups", new ArrayList<>());
+        new FreeMarkerRenderHandler().render("/templates/index.ftl", plugin, objectObjectHashMap);
     }
 }
