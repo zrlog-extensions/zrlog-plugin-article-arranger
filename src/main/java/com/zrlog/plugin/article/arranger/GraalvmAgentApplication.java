72aa6efabcb3509398aea668603bd3614fcf059b
xiaochun <90xchun@gmail.com>
2026-01-29 18:26:32 +0800

    Fix active check error

--- src/main/java/com/zrlog/plugin/article/arranger/GraalvmAgentApplication.java
+++ src/main/java/com/zrlog/plugin/article/arranger/GraalvmAgentApplication.java
@@ -1,12 +1,10 @@
 package com.zrlog.plugin.article.arranger;
 
-import com.google.gson.Gson;
 import com.zrlog.plugin.article.arranger.controller.ArticleArrangerController;
 import com.zrlog.plugin.article.arranger.vo.ArrangeOutlineVO;
 import com.zrlog.plugin.article.arranger.vo.ArticleInfo;
 import com.zrlog.plugin.article.arranger.vo.WidgetDataEntry;
 import com.zrlog.plugin.common.PluginNativeImageUtils;
-import com.zrlog.plugin.data.codec.HttpRequestInfo;
 import com.zrlog.plugin.message.Plugin;
 import com.zrlog.plugin.render.FreeMarkerRenderHandler;
 
@@ -18,17 +16,13 @@ public class GraalvmAgentApplication {
 
 
     public static void main(String[] args) throws IOException {
-        new Gson().toJson(new HttpRequestInfo());
-        new Gson().toJson(new Plugin());
         String basePath = System.getProperty("user.dir").replace("\\target", "").replace("/target", "");
         File file = new File(basePath + "/src/main/resources");
         PluginNativeImageUtils.doLoopResourceLoad(file.listFiles(), file.getPath() + "/", "/");
         PluginNativeImageUtils.exposeController(Collections.singletonList(ArticleArrangerController.class));
         PluginNativeImageUtils.usedGsonObject();
         freemarkerInit();
-        new Gson().toJson(new ArrangeOutlineVO());
-        new Gson().toJson(new ArticleInfo());
-        new Gson().toJson(new WidgetDataEntry());
+        PluginNativeImageUtils.gsonNativeAgentByClazz(Arrays.asList(ArrangeOutlineVO.class, ArticleInfo.class, WidgetDataEntry.class));
         Application.main(args);
 
     }
